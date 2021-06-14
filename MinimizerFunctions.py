0151e92658b19ba69ebc7df315711b98663794a3
Steckmann, Thomas <tmsteckm@ncsu.edu>
2021-06-14 13:00:23 -0400

    Lots of Stuff: * Added Basic Code Packages * Added Documentation for Packages * A bit of Readme Documentation * Additional comments on code
    Documetation Generated using pdoc in the google style. First attempt at rendering documetation

---
+++ MinimizerFunctions.py
@@ -0,0 +1,211 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 14:45:19 2020
+Methods to minimize in order to find the coefficients in the Cartan Decomposition
+
+Two Sections:
+    AdjointRepNorm: Find the zero of |Ad_k(h) - H |, and optimizes over coefficients for k and h
+    AdjointInnerProduct: Find a local minimum of <v, Ad_K(H)>, where v is a dense vector in h
+@author: Thomas Steckmann
+@author: Efekan Kokcu
+"""
+import numpy as np
+import numpy.linalg as la
+import math
+
+from PauliOps import commutatePauliString, multiplyLinComb, simplifyLinComb
+
+
+
+""" ADJOINT REPRESENTATION METHODS """
+
+"""
+Find the zero of |Ad_k(h) - H |, and optimizes over coefficients for k and h
+
+Args:
+    initialGuess: NumPyArray 
+        NumPyArray organized as the coeffcients in order: [k + h] or [k_1 + h' + k_2 + h] or [k_1 + h'' + k_2  +  h'  +  k_3 + h'' + k_4   +   h]
+        length (lenh + lenK) of the guess coefficients
+    *args: Tuple
+        depth: int
+            number of iterations for the Adjoint Action Power series
+        H: NumPyArray
+            Hamiltonian coefficients in the hm basis
+        hm: List of Tuples
+            List of m Pauli String elements, with the h elements first
+        k: List of List of Tuples
+            List of k Pauli String elements, organized as [k, h'', h'] or [k, h''', h'',h'] algebra Tuple Lists
+        h: List of Tuples
+            List of h Pauli String elements
+        
+Return:
+    norm: Float
+        |Ad_k(h) - H |
+"""
+def adjointRepNorm(initialGuess, *args):
+    
+
+    #strips the parameters from *args so we can use in in the sciPy optimizer
+    depth, H, hm, k, h = args
+
+    #Number of parameters to optimize over
+    lenh = len(h)
+    lenk = [len(a) for a in k] #Example: for XY 4 site open, gives [4]. For XY 6 site open, gives [4, 4] corresponding to [len(k), len(h')]
+    lenhm = len(hm)
+    
+    
+    #The input might be multiple sets within a larger k. For instance, k might be decomposed as k1'h'k2'= k.
+    #For this, we need two sets, h' and k', and three sets of coefficients, k1', h', and k2'
+    
+    #Builds the list of k algebra Tuples, to get [k,h,k, h, k,h,k, h...]
+    #Steps as: [] + k + [] => [k] + [h''] + [k] => [kh''k] + [h'] + [kh''k] => [kh''kh'kh''h] 
+    kAlgebraList = []
+    print('DEBUYGGING')
+    print(kAlgebraList)
+    print(k)
+    print()
+    for stepIndex in range(len(k)):
+        kAlgebraList.append(kAlgebraList + k[stepIndex] + kAlgebraList)
+
+    print(kAlgebraList)
+    
+    #Splits the h and k guess from the guess, k is complex but we passed an int so we fix that
+
+    allkCoefficients = initialGuess[lenh:] * 1j
+    hinitialGuess = initialGuess[:lenh]
+    
+    #Further splits k into sections:
+    kCoefficientsList = []
+    buildStepIndices = [] #Temporary array to build up the indices needed to pull apart the input k coefficients
+    for stepIndex in range(len(k)):
+        buildStepIndices = (buildStepIndices + [lenk[stepIndex]] + buildStepIndices)
+    buildStepIndices = [0] + buildStepIndices
+    print(buildStepIndices)
+    buildIndices = [sum(buildStepIndices[:i]) for i in range(len(buildStepIndices))]
+    print(buildIndices)
+    for i in range(buildIndices - 1):
+        kCoefficientsList.append(allkCoefficients[buildIndices[i]:buildIndices[i+1]])
+    
+        
+    
+
+
+
+
+    """    
+    #strips the parameters from *args so we can use in in the sciPy optimizer
+    depth, H, hm, k, h = args
+
+    #Number of parameters to optimize over
+    lenh = len(h)
+    lenk = len(k)
+    lenhm = len(hm)
+    
+    
+    #The input might be multiple sets within a larger k. For instance, k might be decomposed as k1'h'k2'= k.
+    #For this, we need two sets, h' and k', and three sets of coefficients, k1', h', and k2'
+
+
+    #Strips the h and k guess from the guess, k is complex but we passed an int so we fix that
+    hinitialGuess = initialGuess[:lenh]
+    
+    kinitialGuess = initialGuess[lenh:] * 1j
+    """
+    #Padds out empty m terms onto the h Guess so that we can evaluated it using properly sized matricies.  
+    CalcualtedM = np.concatenate((hinitialGuess, np.zeros(lenhm - lenh,dtype=np.complex128)))
+
+
+    for protoIndex in range(len(kAlgebraList)):
+        kAlgebraIndex = len()
+        #adMap generates the ad_k matrix. See the documentation in the adjointAction.py
+        adMap = adjointAction(hm, kAlgebraList[kAlgebraIndex], kCoefficientsList[kAlgebraIndex]) #*2 #########WARNING: I think I fixed this? #########################################################################
+    
+        #The part where we evaluate the resultant Pauli string, CalcualtedM. Repeats depending on the desired depth of adjoint action operations to estimate the adjoint representation
+        #Uses BCH lemma to calculated the value of Ad_k without actually doing any exponentialtion. Basically just a taylor series for the exponential
+        CalcualtedM = adjointRep(depth, adMap, CalcualtedM, lenhm)
+    
+
+    #Distance from the Hamiltonian
+    F = CalcualtedM - H
+    norm = la.norm(F)
+    
+    return norm
+
+
+"""
+Computes the adjoint action operator. 
+This is a matrix which contains the Structure constants of each element in m commuted with each element in k
+Args:
+    hmTupleList: List of Tuples
+        The list of PauliString tuples of all the elements in the M algebra.
+        Ordered so that the first elements are in h, followed by the remaining elements of m
+    kTupleList: List of Tuples
+        The list of PauliString tuples of the elements of the k algebra
+    kVector: list of numbers
+        The vector representation of the k element which generates ad_k.
+        A list of coefficients with indices corresponding to the PauliStrings in kTupleList
+Return:
+    adjointMap: NumPyArray
+        Numpy array of the ad_k map
+Raises:
+    Exception:
+        Mismatched input lengths
+"""
+def adjointAction(hmTupleList,kTupleList,kVector):
+
+    lenK = len(kTupleList)
+    lenkVector = len(kVector)
+    lenHM = len(hmTupleList)
+    if lenK != lenkVector:
+        raise Exception("Mismatched K Vector length")
+    
+    
+    adjointMap = np.zeros((lenHM,lenHM),dtype=np.complex128) #populates the output with a zero array
+    
+    for mCounter in range(lenHM):
+        for indexK in range(lenK):
+            kTuple = kTupleList[indexK]
+            mTuple = hmTupleList[mCounter]
+            coefficient, targetTuple = commutatePauliString(1, kTuple, 1, mTuple)
+            if coefficient == 0:
+                pass
+            else:
+                #The Index in the tuple in the m vector. Actually an integer Index, that gives the location in the matrix
+                hListIndex = hmTupleList.index(targetTuple)
+                #hListIndex = 1
+                adjointMap[(hListIndex, mCounter)] = coefficient * kVector[indexK]
+    return adjointMap
+
+
+"""
+Calculates the Adjoint Representation using the BCH lemma using the adjoint action
+
+Args: 
+    depth: int
+        number of iterations in the taylor series
+    ad: ndarray
+        the matrix represenation of the adjoint action acting on the m space
+    hGuess: numpy array
+        the vector represenation of h in the hm basis
+    lenhm: int
+        the number of elements in the m basis
+
+Return:
+    CalculatedM: numpy array
+        the resultant vector in hm basis
+"""
+def adjointRep(depth, ad, hGuess, lenhm):
+    
+    operatorList = [np.diag(np.ones(lenhm,dtype=np.complex128))]
+    for n in range(depth-1):
+        if n == 0:
+            operatorList.append(operatorList[n] @ ad)
+        else:
+            operatorList.append(operatorList[n] @ ad / (n+1))
+    sumAOperators = sum(operatorList)
+    
+    #Calculates the resulting vector in m = KhK\dagger
+    CalcualtedM = sumAOperators @ hGuess
+    
+    
+ 
---
+++ Optimizer.py
@@ -0,0 +1,375 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 21 14:14:40 2020
+
+General Methods to compute the exponential coefficients in the Cartan Decomposition of a 1D spin lattice hamiltonian
+
+Depends on:
+    - MinimizerFunctions.py
+        - adjointRepNorm
+        - adjointAction
+        - ajointInnerProduct
+        - generalInnerProduct
+        - gradAdjointInnerProduct
+        - gethVecFromk
+        
+    - HamiltonianAlgebra.py
+        - generateAlgebra
+        - hamiltonianTuple
+        - 
+        
+    - CartanMethods.py
+        - MakeGroup (Hamiltonian)
+        - findh(m)
+        - getSubalgebra(m)
+        - getSubalgebraElem(m,element)
+        
+    - PauliOps.py
+        - commutePauliString
+        - multiplyPauliString
+        - multiplyLinComb
+        - simplifyLinComb
+        
+    - IO.py
+        - saveCSV
+        - printList
+
+@author: Thomas Steckmann
+@author: Efekan Kokcu
+"""
+
+#Import General Python Packages
+import numpy as np
+from warnings import warn
+import time
+import csv
+#import os.path
+import scipy.optimize
+#import matplotlib.pyplot as plt
+import random as rand
+
+#Import Package Elements
+from MinimizerFunctions import adjointRepNorm
+from HamiltonianAlgebra import generateAlgebra, hamiltonianTuples
+#from EarpPachosInnerProduct import
+#from CartanMethods import makeGroup, findh, getSubalgebra, getSubalgebraElem
+#from PauliOps import SIGN_RULES, X, Y, Z, I, ops, paulis, commutePauliString, multiplyPauliString, MultiplyLinComb, simplifyLinCom
+#from IO import saveCSV, printList
+
+
+def FindParameters(modelType, J, sites, closed, fileName=None ,depth = 10, acc = 1e-5, options =None):
+    """Hopefully the only function you will need to use. 
+
+    Takes all the parameters of your model and output options and produces the Gates and gate angles
+
+    Args:
+        ModelType (tuple of Strings)
+            Valid models include: 
+                'X',
+                'Y',
+                'Z',
+                'XY',
+                'XY2D',
+                'KitaevEven',
+                'KitaevOdd',
+                'Heisenberg',
+                TransverseIsing',
+                'Transverse_Z'
+            Models can be combined to get different coupling, such as Jx = 0.5 and Jy= 1 being described as ('X','Y')
+        J (tuple of Floats OR Lists of Floats):
+            Depending on the model type, J is either a tuple for coupling corresponding to the same index model,
+            or it is a tuple of tuples with one coupling for each intereaction in the Hamiltonian. Each J is shared by site 
+            for the specificed Hamiltonian                              
+        closed (bool):
+            True if Periodic Boundary conditions
+        fileName (String):                                                                           
+            Determines the read/write file location, if such behavior is desired
+        sites (int):
+            3 - 8 (No hard maximum, just runtime)
+        depth (int): 10
+            The number of terms in the BCH lemma expansion of Adjoint Representation
+            Not needed for EarpPachosInnerProduct
+        acc (float):
+            Determines the convergence criteria
+        options (dict)::
+            {
+            'evolve': string                                                                          #######Not yet implemented###########
+                Choose the path of the optimizer. Default is to use the guess of 1 for all h elements and 0 for all k elements
+                Evolving the Hamiltonian to turn on coupling might be faster or might produce more consistent results. 
+                options:
+                    None
+                    'nextTerm'
+                    'split',
+            'ordered': True,
+            'return': Tuple of strings
+                Determines the output method. Options include:
+                    'python'
+                    'plot'
+                    'mathemtica'                                                                                    
+                    'csv',
+            'debug': bool
+                Print details about the optimization, such as the hamiltonian, algebra, and convergence,        
+            'steps': int
+                The Maximum number of iterations or steps in the optimizer,
+            'statusReporter': bool
+                If True, the program will print progress messages to console
+            'loadFile': bool                                                                       
+            'method': string                                                                 
+                Determines the method of minimization
+                Options include
+                    'AdjointRep'
+                    'AdjointInnerProduct',
+            'optimizerFunction': string
+                The minimizer function to be used. Defaults to Powell for AdjointRep, BFGS for AdjointInnerProduct. Easy to add more options
+                Options:
+                    'Powell'
+                    'BFGS',
+            }
+
+    Todo:
+        * Fix Status Reporter
+        * File Loading
+        * Evolve Optimization Path (Turn on coupling slowly)
+        * File Name Behavior
+        * Cartan Subalgebra Choices
+        * EarpPachosInnerProduct Methods
+            
+    """
+
+
+    """Checking input"""
+    #Input Checking for a few confusing methods
+    if not isinstance(modelType, tuple):
+        raise Exception('modelType must be a Tuple')
+    if not isinstance(J, tuple):
+        raise Exception('J must be a Tuple')
+    if not (isinstance(options,dict) or options is None):
+        raise Exception('Options must be a dictionary or None Type')
+    
+    
+    ########################################
+    """Initialize the options dictionary"""
+    ########################################
+
+    
+    OPTIONS = {'evolve': None, #Not yet implemented
+               'ordered': True,
+               'return': ('python',),
+               'debug': False,
+               'steps': 5000,
+               'statusReporter': False,
+               'loadFile': False, #Not yet implemented
+               'method': 'AdjointInnerProduct', #Not yet implemented
+               'optimizerFunction': 'Powell' #Powell will always work
+               }
+    if OPTIONS['method'] == 'AdjointInnerProduct': #Best for AdjointInnerProduct is BFGS b/c it uses gradient
+        OPTIONS['optimizerFunction'] = 'BFGS'
+    
+    #If user input options are provided, overwrite the default options
+    if options is None:
+        pass
+    else:
+        for key in dict(options): #copies the user specific options to the initialized dictionary
+            OPTIONS[key] = options[key]
+    
+
+    ########################################
+    """Specifies the user specified output"""
+    ########################################
+
+
+    #Intialize Dict of possible methods of returning the solution, initialized to False all except the python output
+    returnMethod = {"python": False, 'plot': False, 'mathematica':False, 'csv': False}
+    
+    #Scans the user input options for return and updates the initialized dict. Also verifies the user input
+    for key in OPTIONS['return']: 
+        if key == 'csv': #Verifies if the fileName is writable and exists if the user wants to save the file
+            if fileName == None:
+                raise Exception("SaveToCSV requires a valid fileName")
+        fileNameCSV = fileName + '.csv'
+        #Test Write
+        with open(fileNameCSV, "w") as f:
+                writer = csv.writer(f)
+                writer.writerows('')
+        f.close()
+        if key == 'loadFile': #Warns the user that loadFile does not do anything
+            warn("File Loading has not yet been implemented", RuntimeWarning)
+        returnMethod[key] = True
+        
+    #If the model is disordered, the user needs to list the desired coupling constants.  This checks that the inputs are correct
+    if not OPTIONS['ordered']:
+        for parameters, model in zip(J,modelType):
+            if not isinstance(parameters, tuple):
+                raise Exception('Disordered systems require Coupling constants listed in a tuple with length appropriate for the number of sites')
+            if model == "TransverseIsing":
+                raise Exception('For disordered transverse Ising, please list the X and Z terms seperately as modelType =("X","Transverse_Z")')
+            elif model == "Transverse_Z" and len(parameters) != (sites):
+                raise Exception('Disordered systems require Coupling constants listed in a tuple with length appropriate for the number of sites')
+            elif len(parameters) != (sites - 1 + closed):
+                raise Exception('Disordered systems require Coupling constants listed in a tuple with length appropriate for the number of sites')
+    else:
+        for parameters in J:
+            if isinstance(parameters, tuple):
+                raise Exception('Ordered system coupling must be specificed for a single Float coupling constant shared by all sites')
+
+
+    #initialized veriables from the OPTIONS dict
+    loadFile = OPTIONS['loadFile']
+    global statusReporter  #Temporary status reporter options. TODO: Allows a count of how many functions have run
+    statusReporter = int(OPTIONS['statusReporter'])
+    debug = OPTIONS['debug']
+    steps = OPTIONS['steps']
+    ordered = OPTIONS['ordered']
+    method = OPTIONS['method']
+    optimizerFunction = OPTIONS['optimizerFunction']
+
+
+    ########################################
+    """Optimizer Implementation - Special cases, algebra generation, and optimizer selection """
+    ########################################
+    
+    
+    """ Special Cases """
+    #There is a special case for the XY model, in which we can use two Kitaev Models instead of one XY model.
+    replaceKitaev = False
+    if "XY" in modelType and (sites % 2 == 0 or not closed) and ordered: #This can be modified later to allow for disordered systems specified by one list of coupling (shared Jx and Jy at each site)
+        replaceKitaev = True
+    
+    
+    """ Algebra Generation"""
+    #Generates the populated coefficient Hamiltonian in the m algebra basis, finds the hm algebra (h terms followed by m terms)
+    #, the k and the h algebras using a function call. See documentation in GenerateHamiltonian.py
+    Hcoeff = None
+    if replaceKitaev: #Instead of running 1 XY, run 1 Kitaev, and use that result to generate the Kitaev result (saves time)
+        if method =='AdjointInnerProduct':
+            H, hm, k, h  = generateAlgebra(sites, ('KitaevEven',), J, closed)
+            HTuples = hamiltonianTuples(sites, 'KitaevEven', closed)
+            Hcoeff = np.ones(len(HTuples)) * J[0]
+        else:
+            H, hm, k, h  = generateAlgebra(sites, ('KitaevEven',), J, closed)
+    elif method =='AdjointInnerProduct':
+        H, hm, k, h  = generateAlgebra(sites, modelType, J, closed) 
+        HTuples = hamiltonianTuples(sites, modelType, closed)
+        Hcoeff = np.ones(len(HTuples)) * J[0]
+    else:
+        H, hm, k, h  = generateAlgebra(sites, modelType, J, closed) 
+
+    
+    #Reports on the size of the optimization, the parameters, and the algebras if the user specifies
+    if statusReporter:
+        if method == ['AdjointInnerProduct']:
+            print("Beginning minimization over %s parameters" % len(k))
+        else:
+            print("Beginning optimization over %s parameters" % len(hm))
+    if debug:
+        print('h = ')
+        print(h)
+        print('hm = ')
+        print(hm)
+        print('k = ')
+        print(k)
+    
+    
+    #Intial Guess: 1 for all h terms, 0 for all k Terms
+    if method == 'AdjointRep':
+        initialGuess = np.concatenate((np.ones(len(h)), np.zeros(len(k))))      
+    elif method == 'AdjointInnerProduct':
+        initialGuess = np.zeros(len(k))
+    else:
+        initialGuess = None
+    
+    ################################################################################################
+    """The Optimizer. Defined internally to allow for easy customization based on the method options
+       https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.minimize.html"""
+    ################################################################################################
+    def optimize(initialGuess):
+        start_time = time.time()
+        #Passes the Hamiltonian through the args, calle the "function" to minimize using the initial Guess we pass it initially
+        """Toggle options for the minimizer"""
+        #Currently, we only use the Powell method. In the future, we can add an option to change method for minimization
+        if method == 'adjointAction':
+            optimiumReturn = scipy.optimize.minimize(adjointRepNorm,initialGuess, args = (depth, H, hm, k, h), method=optimizerFunction,options={'disp':(not statusReporter == 0), 'ftol':acc, 'maxiter':steps})# "xtol": accuracy})#, 'gtol':1e-20 ,'maxiter':500})
+        elif method == 'AdjointInnerProduct':
+            pass
+
+            ########################################################
+            ########################################################
+            #Efekan Code Goes Here
+            ########################################################
+            ########################################################
+
+        else:
+            optimiumReturn = None
+            raise Exception('Incorrect Method')
+        print("--- %s seconds ---" % (time.time() - start_time))
+        
+        return optimiumReturn
+    
+    #runs the optimizer functions
+    result = optimize(initialGuess)
+    x = result.x #See https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.OptimizeResult.html#scipy.optimize.OptimizeResult
+    print(x)
+    
+    print('Printing E&P k results')
+    if method == 'AdjointInnerProduct':
+        print([b for b in zip(x, k)])
+    
+    
+    #Special Cases combining two Kitaev Models into one XY model
+    if replaceKitaev:
+        #evenAlgebra
+        H0, hm0, k0, h0  = generateAlgebra(sites, ('KitaevEven',), J, closed)
+        #oddAlgebra 
+        H1, hm1, k1, h1  = generateAlgebra(sites, ('KitaevOdd',), J, closed)
+        #The k coefficients are negated, while the h coefficients are shared
+        xh0 = x[:-len(k)]
+        xk0 = x[len(h):]
+        xh1 =  1 * xh0
+        xk1 = -1 * xk0
+        #full solution
+        h = h0 + h1
+        k = k0 + k1
+        hm = hm0 + hm1
+
+        x = np.concatenate((xh0 , xh1 , xk0 , xk1))
+        H, a, b, c = generateAlgebra(sites, modelType, J, closed)
+        #The algebra terms are now returned to normal and we can verify the solution
+
+    ########################################################################
+    """OUTPUT processing and saving"""
+    ########################################################################
+
+    #Always prints the result
+    print(hm)
+    print(x)
+    if returnMethod['python']:
+        print([a for a in zip(x[:len(h)], h)])
+        print([b for b in zip(x[ len(h):], k)])
+
+
+    #Selects which outputs to run
+    if returnMethod['mathematica']:    
+        pass
+        #mathematicaString = buildMathematicaResult(results,hm, h, k)
+    if returnMethod['csv']:
+        #Saves as CSV
+        #Format: 
+        #'h and k algebra' 'coefficients' 'hm algebra' 'hamiltonian'
+        # Tuple              Float           Tuple        Float
+        fileNameCSV = fileName + '.csv'
+        #Converts the formatting
+        solutionList = [['h and k algebra', 'coefficients', 'hm algebra' ,'hamiltonian']]
+        for hkAlgebra, coefficient, hmAlgebra, hamiltonian in zip(h + k,x,hm, H):
+            solutionList.append([hkAlgebra, coefficient, hmAlgebra, hamiltonian])
+        with open( fileNameCSV, "w", newline='') as f:
+                writer = csv.writer(f)
+                writer.writerows(solutionList)
+        f.close()
+    return x[:-len(k)] , h, x[ len(h):], k
+
+
+
+#sites = 4
+#ID = 'ML_Data\\MLdata_' + '4_XY_Open_OldMethod_file1'
+#FindParameters(('XY',),(1,),sites,False, fileName= ID ,options = {'statusReporter': True, 'debug': True, 'return': ('python', 'csv'), 'method': 'adjointAction'})
+#print(ID)
