6361a372191e794b2d2380f7a4892b0354522997
Thomas Steckmann <67061034+tmsteck@users.noreply.github.com>
2021-06-23 11:31:02 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,16 @@
 from setuptools import setup
 
 setup(
-    # Needed to silence warnings (and to be a worthwhile package)
     name='CQS',
     url='https://github.com/kemperlab/cartan-quantum-synthesizer',
     author='Thomas Steckmann, Efekan Kokcu',
     author_email='tmsteckm@ncsu.edu, ekokcu@ncsu.edu',
-    # Needed to actually package something
     packages=['CQS.util'],
     project_urls={
-        "Documtation": "https://kemperlab.github.io/cartan-quantum-synthesizer/",
+        "Documentation": "https://kemperlab.github.io/cartan-quantum-synthesizer/",
     },
-    # Needed for dependencies
     install_requires=['numpy','scipy'],
-    # *strongly* suggested for sharing
     version='0.2',
-    # The license can be anything you like
-    license='MIT',
+    license='BSD-2-Clause Plus Patent License',
     description='Implementation of the Cartan Decomposition for generating time evolution circuits on lattice spin models',
 )
